3f993e3d44349d972da64f4cc3aabc98d3b30b65
Nurzamal Islam <n.zaman98@gmail.com>
2025-02-11 22:21:17 +0530

    sonar issue for nextline

--- src/main/java/com/nur/service/impl/RatingActionServiceImpl.java
+++ src/main/java/com/nur/service/impl/RatingActionServiceImpl.java
@@ -12,7 +12,6 @@ import com.nur.service.RatingActionService;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,17 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class RatingActionServiceImpl implements RatingActionService {
 
-    @Autowired
-    CrrEmailNotificationRepository crrEmailNotificationRepository;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(RatingActionServiceImpl.class);
 
     private final RatingActionRepository repository;
+    private final CrrEmailNotificationRepository crrEmailNotificationRepository;
     private final ModelMapper modelMapper;
     private final EmailService emailService;
 
-    public RatingActionServiceImpl(RatingActionRepository repository, ModelMapper modelMapper, EmailService emailService) {
+    public RatingActionServiceImpl(RatingActionRepository repository, CrrEmailNotificationRepository crrEmailNotificationRepository, ModelMapper modelMapper, EmailService emailService) {
         this.repository = repository;
+        this.crrEmailNotificationRepository = crrEmailNotificationRepository;
         this.modelMapper = modelMapper;
         this.emailService = emailService;
     }
@@ -51,10 +49,11 @@ public class RatingActionServiceImpl implements RatingActionService {
         CrrEmailNotificationEntity emailDetails = crrEmailNotificationRepository.findAll().get(0);
 
         EmailDTO emailDTO = new EmailDTO();
+        emailDTO.setCountry(dto.getCountry());
         emailDTO.setEmailFrom(emailDetails.getEmailFrom());
         emailDTO.setEmailTo(emailDetails.getEmailTo());
         emailDTO.setEmailSubject(emailDetails.getEmailSubject());
-        emailDTO.setEmailBody(emailDetails.getEmailBody() + String.format("%n%n") + getEmailContent(dto));
+        emailDTO.setEmailBody(emailDetails.getEmailBody()+ "-" + emailDTO.getCountry() + String.format("%n%n") + getEmailContent(dto));
         emailDTO.setEmailSignature(emailDetails.getEmailSignature());
         emailDTO.setCurrentCRR(String.valueOf(dto.getOldCrr()));
         emailDTO.setProposedCRR(String.valueOf(dto.getNewCrr()));
@@ -64,21 +63,17 @@ public class RatingActionServiceImpl implements RatingActionService {
     }
 
     private String getEmailContent(RatingDTO dto) {
-        return String.format("""
-    <html>
-    <body> <br>
-        <table border="1" cellpadding="5" cellspacing="0" width="70%%" style="border-collapse: collapse; border: 1px solid gray;">
-            <tr style="text-align: left;"><th>Field Name</th><th>Old Value</th><th>New Value</th></tr>
-            <tr><td>CRR</td><td>%s</td><td>%s</td></tr>
-            <tr><td>CRR Outlook</td><td>%s</td><td>%s</td></tr>
-        </table>
-    </body>
-    </html>
-    """,
-                dto.getOldCrr(),
-                dto.getNewCrr(),
-                dto.getOldCrrOutlook(),
-                dto.getNewCrrOutlook());
+        return "<html>" +
+                "<body> <br>" +
+                "<table border=\"1\" cellpadding=\"5\" cellspacing=\"0\" width=\"70%\" style=\"border-collapse: collapse; border: 1px solid gray;\">" +
+                "<tr style=\"text-align: left;\"><th>Field Name</th><th>Old Value</th><th>New Value</th></tr>" +
+                String.format("<tr><td>CRR</td><td>%s</td><td>%s</td></tr>%n", dto.getOldCrr(), dto.getNewCrr()) +
+                String.format("<tr><td>CRR Outlook</td><td>%s</td><td>%s</td></tr>%n", dto.getOldCrrOutlook(), dto.getNewCrrOutlook()) +
+                "</table>" +
+                "</body>" +
+                "</html>";
     }
+
+
 }
 
